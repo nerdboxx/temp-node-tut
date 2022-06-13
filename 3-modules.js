30a3066bea3519ac5bfe81489bd18b626ca742b2
nerdboxx <nerdbox1@googlemail.com>
2022-06-13 21:18:33 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// CommonJS, every file is module (by default)
+// Modules
+const names = require('./4-firstmodules') 
+const sayHi = require('./5-secondmodule')
+const data = require('./6-alternative-flavor')
+require('./7-mindgrenade')
+
+//sayHi('susan')
+//sayHi(names.john)
+//sayHi(names.peter)
